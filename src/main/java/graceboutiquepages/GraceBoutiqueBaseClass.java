dba4ceb4a30b9d37d61d48a6600be24bab465f09
AverilRozario <averilrozario187@gmail.com>
2024-12-20 18:09:53 +0530

    Update GraceBoutiqueBaseClass.java

--- src/main/java/graceboutiquepages/GraceBoutiqueBaseClass.java
+++ src/main/java/graceboutiquepages/GraceBoutiqueBaseClass.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class GraceBoutiqueBaseClass {
