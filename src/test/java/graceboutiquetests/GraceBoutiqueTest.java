d23ec2de6b36ef10b38886c330d8e29d7fbc1d37
AverilRozario <averilrozario187@gmail.com>
2024-12-20 17:59:26 +0530

    Update GraceBoutiqueTest.java

--- src/test/java/graceboutiquetests/GraceBoutiqueTest.java
+++ src/test/java/graceboutiquetests/GraceBoutiqueTest.java
@@ -13,40 +13,44 @@ public class GraceBoutiqueTest extends GraceBoutiqueBaseClass{
 	public void GBTest () throws InterruptedException, IOException
 	{
 		GraceBoutiquePage GB=new GraceBoutiquePage(driver);
-
+		
+        	// Verify title
 		GB.titleVerification();
 		
+        	// Verify logo visibility
 		GB.verifyLogo();
-		
+
+		// Verify link count and broken links
 		GB.linkCount();
 
+		// Verify page source contains specific text
 		GB.pageSource();
-		
+
+		// Test user registration and login flow
 		GB.loginIcon();
-		
 		GB.createAccount();
 		GB.registerSetValues("Averil", "Rozario", "averilrozario187@gmail.com", "9746598710", "aq@E1wr2", "aq@E1wr2");
 		GB.registerClick();
-		
+
+		// Login after registration
 		GB.login();
-		
 		GB.loginSetValues("averilrozario187@gmail.com", "aq@E1wr2");
 		GB.loginClick();
 		
 		//GB.alertHandle();
-		
+
+		// Scroll and interact with page elements
 		GB.scrollDown();
-		
 		GB.moveToElement();
-		
+
+		// Perform search and item interaction
 		GB.searchBar();
-		
 		GB.salwarClick();
-		
 		GB.sortBy();
-		
 		GB.itemClick();
 		GB.addToCart();
+
+		// Capture screenshot
 		GB.screenShot();
 	}
 	
